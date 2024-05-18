f1aa7fcc151be0b96c4e2f821511b6885ad01ea5
SkrebkovMihail <151344272+SkrebkovMihail@users.noreply.github.com>
2024-05-18 13:14:16 +0300

    ?

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,43 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_BST_H_
-#define INCLUDE_BST_H_
+// bst.h
+#ifndef BST_H
+#define BST_H
 
-#endif  // INCLUDE_BST_H_
+#include <string>
+
+
+template<typename T>
+struct Node {
+    T data;
+    int count;
+    Node* left;
+    Node* right;
+
+
+    Node(const T& value) : data(value), count(1), left(nullptr), right(nullptr) {}
+};
+
+
+template<typename T>
+class BST {
+private:
+    Node<T>* root;
+
+
+    void destroyTree(Node<T>* node);
+    Node<T>* insert(Node<T>* node, const T& value);
+    void inOrderTraversal(Node<T>* node, std::ostream& os) const;
+
+public:
+
+    BST();
+    ~BST();
+
+
+    void insert(const T& value);
+    void printInOrder(std::ostream& os) const;
+};
+
+#include "alg.cpp"
+
+#endif // BST_H
