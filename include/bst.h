fd61fd1fbbba010ce98932e1d9a64d3b373ae366
SkrebkovMihail <151344272+SkrebkovMihail@users.noreply.github.com>
2024-05-18 13:18:35 +0300

    ?

--- include/bst.h
+++ include/bst.h
@@ -1,43 +1,82 @@
 // Copyright 2021 NNTU-CS
-// bst.h
-#ifndef BST_H
-#define BST_H
+#ifndef INCLUDE_BST_H_
+#define INCLUDE_BST_H_
+#include <iostream>
 
-#include <string>
-
-
-template<typename T>
-struct Node {
-    T data;
-    int count;
-    Node* left;
-    Node* right;
-
-
-    Node(const T& value) : data(value), count(1), left(nullptr), right(nullptr) {}
-};
-
-
-template<typename T>
+template <typename T>
 class BST {
 private:
-    Node<T>* root;
-
-
-    void destroyTree(Node<T>* node);
-    Node<T>* insert(Node<T>* node, const T& value);
-    void inOrderTraversal(Node<T>* node, std::ostream& os) const;
-
+    struct Node {
+        T word;
+        int count;
+        Node* left, * right;
+    };
+    Node* root = nullptr;
+    Node* addNodeTree(Node*, const T&);
+    int searchNodeTree(Node*, const T&);
+    int getDepthTree(Node*);
 public:
-
-    BST();
-    ~BST();
-
-
-    void insert(const T& value);
-    void printInOrder(std::ostream& os) const;
+    void addNode(const T&);
+    int search(const T&);
+    int depth();
 };
 
-#include "alg.cpp"
 
-#endif // BST_H
+template<class T>
+typename BST<T>::Node* BST<T>::addNodeTree(Node* root, const T& word_) {
+    if (root == nullptr) {
+        root = new Node;
+        root->word = word_;
+        root->count = 1;
+        root->left = root->right = nullptr;
+    } else if (root->word > word_) {
+        root->left = addNodeTree(root->left, word_);
+    } else if (root->word < word_) {
+        root->right = addNodeTree(root->right, word_);
+    } else {
+        root->count++;
+    }
+    return root;
+}
+
+template<class T>
+void BST<T>::addNode(const T& word_) {
+    root = addNodeTree(root, word_);
+}
+
+template<class T>
+int BST<T>::searchNodeTree(Node* root, const T& word_) {
+    if (root == nullptr) {
+        return 0;
+    } else if (root->word > word_) {
+        return searchNodeTree(root->left, word_);
+    } else if (root->word < word_) {
+        return searchNodeTree(root->right, word_);
+    } else {
+        return root->count;
+    }
+    return 0;
+}
+
+template<class T>
+int BST<T>::search(const T& word_) {
+    return searchNodeTree(root, word_);
+}
+
+template<class T>
+int BST<T>::getDepthTree(Node* root) {
+    if (root == nullptr)
+        return 0;
+    int leftDepth = getDepthTree(root->left);
+    int rightDepth = getDepthTree(root->right);
+    if (leftDepth > rightDepth)
+        return leftDepth + 1;
+    else
+        return rightDepth + 1;
+}
+
+template<class T>
+int BST<T>::depth() {
+    return getDepthTree(root) - 1;
+}
+#endif  // INCLUDE_BST_H_
--- src/alg.cpp
+++ src/alg.cpp
@@ -1,99 +1,38 @@
 // Copyright 2021 NNTU-CS
-#include "bst.h"
-#include <iostream>
-#include <fstream>
-#include <cctype>
-
-
-template<typename T>
-BST<T>::BST() : root(nullptr) {}
-
-
-template<typename T>
-BST<T>::~BST() {
-    destroyTree(root);
-}
-
-
-template<typename T>
-void BST<T>::destroyTree(Node<T>* node) {
-    if (node != nullptr) {
-        destroyTree(node->left);
-        destroyTree(node->right);
-        delete node;
-    }
-}
-
-
-template<typename T>
-void BST<T>::insert(const T& value) {
-    root = insert(root, value);
-}
-
-
-template<typename T>
-Node<T>* BST<T>::insert(Node<T>* node, const T& value) {
-    if (node == nullptr) {
-        return new Node<T>(value);
-    }
-
-    if (value < node->data) {
-        node->left = insert(node->left, value);
-    } else if (value > node->data) {
-        node->right = insert(node->right, value);
-    } else {
-
-        node->count++;
-    }
-
-    return node;
-}
-
-
-template<typename T>
-void BST<T>::inOrderTraversal(Node<T>* node, std::ostream& os) const {
-    if (node != nullptr) {
-        inOrderTraversal(node->left, os);
-        os << node->data << " - " << node->count << std::endl;
-        inOrderTraversal(node->right, os);
-    }
-}
-
-
-template<typename T>
-void BST<T>::printInOrder(std::ostream& os) const {
-    inOrderTraversal(root, os);
-}
-
+#include  <iostream>
+#include  <fstream>
+#include  <locale>
+#include  <cstdlib>
+#include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-    BST<std::string> tree;
-
-
     std::ifstream file(filename);
-
-
-    if (!file) {
-        throw std::runtime_error("Error opening file");
-    }
-
-
+    BST<std::string> tree;
     std::string word;
-    while (file >> word) {
 
-        for (char& c : word) {
-            c = std::tolower(c);
+    if (file) {
+        while (!file.eof()) {
+            word.erase();
+            while (1) {
+                char symbol = file.get();
+                if (symbol > 96 && symbol < 123) {
+                    word += symbol;
+                } else if (symbol > 64 && symbol < 91) {
+                    symbol += 32;
+                    word += symbol;
+                } else {
+                    break;
+                }
+            }
+            if (!word.empty()) {
+                tree.addNode(word);
+            } else {
+                continue;
+            }
         }
-
-        word.erase(std::remove_if(word.begin(), word.end(), [](char c) { return !std::isalpha(c); }), word.end());
-
-        if (!word.empty()) {
-            tree.insert(word);
-        }
+        file.close();
+    } else {
+        std::cout << "File was not read correctly." << std::endl;
     }
-
-
-    file.close();
-
     return tree;
 }
